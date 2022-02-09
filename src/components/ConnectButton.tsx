20fa3f3e0b39e3172fa402885557dc1a5351d7df
0xluckyg <scottsgcho@gmail.com>
2022-02-09 23:24:49 +0900

    connect button

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,4 +1,4 @@
-import { useWeb3React } from "@web3-react/core"
+import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core"
 
 import { getErrorMessage } from "@helper/getErrorMessage"
 import { switchChains } from "@helper/walletHelpers"
@@ -24,7 +24,7 @@ function ConnectButton(props: ownProps) {
     return (
       <div className={customStyle ? customStyle : ""}>
         <div className="flex items-center font-semibold 2xl:text-xl gap-2 text-dark-800 tracking-2%">
-          {active || (
+          {active && (
             <div className="tracking-2% md:text-sm 2xl:text-base">
               <div className="flex items-center gap-3">
                 <span className="uppercase text-dark-800">Wallet</span>
@@ -41,7 +41,7 @@ function ConnectButton(props: ownProps) {
 
   return (
     <div className={customStyle ? customStyle : ""}>
-      {error ? (
+      {error instanceof UnsupportedChainIdError ? (
         <button
           type="button"
           className="bg-black sm:text-xs font-bold rounded-xl border border-gray-600 button 2xl:button-lg button-sm button-primary button-hover"
@@ -67,3 +67,71 @@ function ConnectButton(props: ownProps) {
 }
 
 export default ConnectButton
+
+// import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core"
+
+// import ConnectorModal from "./modals/ConnectorModal"
+
+// interface ownProps {
+//   customStyle?: string
+// }
+
+// function ConnectButton(props: ownProps) {
+//   const { customStyle } = props
+
+//   const { account, active, error } = useWeb3React()
+
+//   const { showModal } = useModal()
+//   const accountFormatted = account?.substring(0, 6) + "..."
+
+//   const errorMessage = getErrorMessage(error)
+
+//   if (active) {
+//     return (
+//       <div className="flex items-center font-semibold 2xl:text-xl gap-2 text-dark-800 tracking-2%">
+//         {active && (
+//           <div className="tracking-2% md:text-sm 2xl:text-base">
+//             <div className="flex items-center gap-3">
+//               <span className="uppercase text-dark-800">Wallet</span>
+//               <span className="w-3 h-3 rounded-full bg-[#cbd3cc]"></span>
+//             </div>
+
+//             <p className="text-dark-100">{accountFormatted}</p>
+//           </div>
+//         )}
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <div className={customStyle ? customStyle : ""}>
+//       <div className="2xl:px-6">
+//         <button
+//           type="button"
+//           className="bg-black sm:text-xs font-bold rounded-xl border border-gray-600 w-full button 2xl:button-lg button-sm button-primary button-hover"
+//           onClick={() => {
+//             showModal(<ConnectorModal />)
+//           }}
+//         >
+//           Connect Wallet
+//         </button>
+//       </div>
+//       {error && (
+//         <div className="mt-3">
+//           {error instanceof UnsupportedChainIdError ? (
+//             <button
+//               onClick={switchChains}
+//               className="text-xs font-medium text-left text-gray-200"
+//             >
+//               Incorrect Chain! Click to switch
+//             </button>
+//           ) : (
+//             <p className="text-xs font-medium text-gray-200">{errorMessage}</p>
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default ConnectButton

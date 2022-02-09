0760a49d3301aec687335b99f09d1e523bb805f5
0xluckyg <scottsgcho@gmail.com>
2022-02-09 23:12:26 +0900

    connect button change

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,4 +1,4 @@
-import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core"
+import { useWeb3React } from "@web3-react/core"
 
 import { getErrorMessage } from "@helper/getErrorMessage"
 import { switchChains } from "@helper/walletHelpers"
@@ -22,46 +22,44 @@ function ConnectButton(props: ownProps) {
 
   if (active) {
     return (
-      <div className="flex items-center font-semibold 2xl:text-xl gap-2 text-dark-800 tracking-2%">
-        {active && (
-          <div className="tracking-2% md:text-sm 2xl:text-base">
-            <div className="flex items-center gap-3">
-              <span className="uppercase text-dark-800">Wallet</span>
-              <span className="w-3 h-3 rounded-full bg-[#cbd3cc]"></span>
-            </div>
+      <div className={customStyle ? customStyle : ""}>
+        <div className="flex items-center font-semibold 2xl:text-xl gap-2 text-dark-800 tracking-2%">
+          {active || (
+            <div className="tracking-2% md:text-sm 2xl:text-base">
+              <div className="flex items-center gap-3">
+                <span className="uppercase text-dark-800">Wallet</span>
+                <span className="w-3 h-3 rounded-full bg-[#cbd3cc]"></span>
+              </div>
 
-            <p className="text-dark-100">{accountFormatted}</p>
-          </div>
-        )}
+              <p className="text-dark-100">{accountFormatted}</p>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
 
   return (
     <div className={customStyle ? customStyle : ""}>
-      <div className="2xl:px-6">
+      {error ? (
         <button
           type="button"
           className="bg-black sm:text-xs font-bold rounded-xl border border-gray-600 button 2xl:button-lg button-sm button-primary button-hover"
-          onClick={() => {
-            showModal(<ConnectorModal />)
-          }}
+          onClick={switchChains}
         >
-          Connect Wallet
+          Switch Chain
         </button>
-      </div>
-      {error && (
-        <div className="mt-3">
-          {error instanceof UnsupportedChainIdError ? (
-            <button
-              onClick={switchChains}
-              className="text-xs font-medium text-left text-gray-200"
-            >
-              Incorrect Chain! Click to switch
-            </button>
-          ) : (
-            <p className="text-xs font-medium text-gray-200">{errorMessage}</p>
-          )}
+      ) : (
+        <div className="2xl:px-6">
+          <button
+            type="button"
+            className="bg-black sm:text-xs font-bold rounded-xl border border-gray-600 button 2xl:button-lg button-sm button-primary button-hover"
+            onClick={() => {
+              showModal(<ConnectorModal />)
+            }}
+          >
+            Connect Wallet
+          </button>
         </div>
       )}
     </div>
